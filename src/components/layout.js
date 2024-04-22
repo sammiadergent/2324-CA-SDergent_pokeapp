6e78a0872311956305e399fbefb535ac446b9c4f
Sammïa Dergent <sammiadergent@hotmail.com>
2024-04-22 11:28:44 +0200

    uitgevogeld hoe ik svg moet toevoegen

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,15 @@
 import Link from 'next/link';
+import styles from '@/styles/Layout.module.css';
+import Bovenkader from '../achtergrond/bovenkader.svg';
 
 export default function Layout({ children }) {
   return (
     <>
+      <div className={styles.header}>
+        <Bovenkader width="50%" />
+      </div>
       <nav>
         <Link href="/">Home</Link>
-        <Link href="/">About</Link>
       </nav>
       <main>{children}</main>
     </>
